1be6e03424bc5adfabf6eaa1390345a5c4870b91
Debargha Mondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-07-29 15:48:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function firstChar(text) {
   // your code here
 let letter = text.charAt(0);
+	return letter;
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
